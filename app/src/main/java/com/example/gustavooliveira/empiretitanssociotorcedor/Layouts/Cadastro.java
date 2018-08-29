912f33b235186a1a4e38cdc8aef3753ba0ac4f66
Gustavo Oliveira <gtv.oliveira@gmail.com>
2018-08-28 21:26:36 -0300

    Campos Cadastro tratados

--- app/src/main/java/com/example/gustavooliveira/empiretitanssociotorcedor/Layouts/Cadastro.java
+++ app/src/main/java/com/example/gustavooliveira/empiretitanssociotorcedor/Layouts/Cadastro.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.gustavooliveira.empiretitanssociotorcedor.R;
@@ -14,6 +15,16 @@ public class Cadastro extends AppCompatActivity {
 
     private Button btConfirma;
     private Button btCancelar;
+    private EditText txtNome;
+    private EditText txtSobrenome;
+    private EditText txtEmail;
+    private EditText txtData;
+    private EditText txtCpf;
+    private EditText txtTelefone;
+    private EditText txtEndereco;
+    private EditText txtSenha;
+    private EditText txtSenhaConfirm;
+    private EditText txtCartao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +34,21 @@ public class Cadastro extends AppCompatActivity {
 
         btConfirma = (Button)findViewById(R.id.btConfirma);
         btCancelar = (Button)findViewById(R.id.btCancelar);
+        txtNome = (EditText)findViewById(R.id.txtNome);
+        txtSobrenome = (EditText)findViewById(R.id.txtSobrenome);
+        txtEmail = (EditText)findViewById(R.id.txtEndereco);
+        txtData = (EditText)findViewById(R.id.txtData);
+        txtCpf = (EditText)findViewById(R.id.txtCpf);
+        txtTelefone = (EditText)findViewById(R.id.txtTelefone);
+        txtEndereco = (EditText)findViewById(R.id.txtEndereco);
+        txtSenha = (EditText)findViewById(R.id.txtSenha);
+        txtSenhaConfirm = (EditText)findViewById(R.id.txtSenhaConfirm);
+        txtCartao = (EditText)findViewById(R.id.txtCartao);
 
         btConfirma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Botão para a função criar novo usuario
                 Toast.makeText(getApplicationContext(), "Função não criada", Toast.LENGTH_LONG).show();
             }
         });
@@ -36,6 +58,7 @@ public class Cadastro extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Cadastro.this, Login.class);
                 startActivity(intent);
+                finish();
             }
         });
 
--- app/src/main/java/com/example/gustavooliveira/empiretitanssociotorcedor/Layouts/Login.java
+++ app/src/main/java/com/example/gustavooliveira/empiretitanssociotorcedor/Layouts/Login.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.gustavooliveira.empiretitanssociotorcedor.MainActivity;
 import com.example.gustavooliveira.empiretitanssociotorcedor.R;
