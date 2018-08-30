f1d2183ec1ddaaa6d72f8f75e765128621b38ada
Willian <willian417@outlook.com>
2018-08-30 00:09:23 -0300

    Alterações para implementação do cadastro do usuário.

---
+++ app/src/main/java/com/example/gustavooliveira/empiretitanssociotorcedor/Salesforce/UsuarioSF.java
@@ -0,0 +1,31 @@
+package com.example.gustavooliveira.empiretitanssociotorcedor.Salesforce;
+
+import com.example.gustavooliveira.empiretitanssociotorcedor.Models.Usuario;
+
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class UsuarioSF {
+
+    public void Cadastrar(Usuario usuario) throws Exception {
+
+        HttpURLConnection conexao = (HttpURLConnection) new URL("https://na57.salesforce.com/services/data/v20.0/sobjects/Usuario").openConnection();
+        conexao.setDoInput(true);
+        conexao.setDoOutput(true);
+        conexao.setRequestMethod("POST");
+        conexao.setRequestProperty("Authorization", "Bearer " + Autenticacao.get().getToken());
+        conexao.setRequestProperty("Content-Type", "application/json");
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(conexao.getOutputStream(), "UTF-8"));
+        writer.write(usuario.toJSON(false).toString());
+        writer.flush();
+        writer.close();
+
+        if(conexao.getResponseCode() != HttpURLConnection.HTTP_OK)
+            throw new Exception(conexao.getResponseMessage());
+    }
+
+}
