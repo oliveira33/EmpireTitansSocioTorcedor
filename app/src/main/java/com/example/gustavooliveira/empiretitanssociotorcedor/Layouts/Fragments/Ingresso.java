f5fee25833d40c72a19a3d083fe6c257faa97646
Willian <willian417@outlook.com>
2018-09-29 13:11:25 -0300

    Correção da listagem de partidas

--- app/src/main/java/com/example/gustavooliveira/empiretitanssociotorcedor/Layouts/Fragments/Ingresso.java
+++ app/src/main/java/com/example/gustavooliveira/empiretitanssociotorcedor/Layouts/Fragments/Ingresso.java
@@ -1,27 +1,20 @@
 package com.example.gustavooliveira.empiretitanssociotorcedor.Layouts.Fragments;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
 import android.widget.Toast;
 
 import com.example.gustavooliveira.empiretitanssociotorcedor.Adapters.IngressoAdapter;
 import com.example.gustavooliveira.empiretitanssociotorcedor.Models.Partida;
 import com.example.gustavooliveira.empiretitanssociotorcedor.R;
-import com.example.gustavooliveira.empiretitanssociotorcedor.Salesforce.DateSF;
 import com.example.gustavooliveira.empiretitanssociotorcedor.Salesforce.PartidaSF;
 
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,7 +22,7 @@ import java.util.List;
 public class Ingresso extends Fragment {
 
     private RecyclerView recyclerView;
-    static List<Partida> listPartidas = new ArrayList<>();
+    private ArrayList<Partida> listPartidas = new ArrayList<>();
     private View mView;
     private IngressoAdapter adapter;
 
@@ -42,30 +35,38 @@ public class Ingresso extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        preencherListPartidas();
         mView = inflater.inflate(R.layout.fragment_ingresso, container, false);
         recyclerView = mView.findViewById(R.id.recycleView_Ingresso);
-        adapter = new IngressoAdapter(listPartidas, getContext());
         RecyclerView.LayoutManager li = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         recyclerView.setLayoutManager(li);
-        recyclerView.setAdapter(adapter);
+
+        consultarPartidas();
 
         return mView;
     }
 
-    private void preencherListPartidas() {
+    private void consultarPartidas() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    listPartidas = new PartidaSF().getProximasPartidas();
+                    preencherPartidas(listPartidas);
+                } catch (Exception e) {
+                    Toast.makeText(getContext(), "A lista de ingressos não pode ser carregada.\n" + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }.start();
+    }
 
-        /*try {
-            listPartidas = new PartidaSF().getProximasPartidas();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(getContext(), "A lista de ingressos não pode ser carregada.\n"+e, Toast.LENGTH_LONG).show();
-        }*/
-        listPartidas.add(new Partida("5", "1", new Date(), 10.50, "oi"));
-        listPartidas.add(new Partida("1", "1", new Date(), 10.50, "oi"));
-        listPartidas.add(new Partida("5", "1", new Date(), 10.50, "oi"));
-        listPartidas.add(new Partida("1", "1", new Date(), 10.50, "oi"));
+    private void preencherPartidas(final ArrayList<Partida> partidas) {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                adapter = new IngressoAdapter(partidas, getContext());
+                recyclerView.setAdapter(adapter);
+            }
+        });
     }
 
 }
